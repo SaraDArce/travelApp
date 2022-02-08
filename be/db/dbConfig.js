9f0d0a6fc95b0a3258e8ba7e4adc8810cfff0753
Sara D'Arce <saradarce@gmail.com>
2022-02-08 13:05:10 -0500

    db seed schema

---
+++ be/db/dbConfig.js
@@ -0,0 +1,13 @@
+const pgPromise = require("pg-promise")();
+require("dotenv").config();
+
+const db = pgPromise({
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  database: process.env.PG_DATABASE,
+  user: process.env.PG_USER,
+});
+
+// console.log("Trigger"); // This will run! Can Test db.any("SELECT * FROM places;", etc.....)
+
+module.exports = db;
---
+++ be/readme.md
@@ -0,0 +1,13 @@
+
+
+Database name: travel_dev
+
+Table name: places
+
+name, 
+city, 
+state, 
+country, 
+average_temp
+hasBeaches
+publicTransportationQuality
